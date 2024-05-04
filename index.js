a9d0e4003be7aa070dbe01d1f571e87a25e7bffb
BenStephen23 <benstephe 2000@gmail.com>
2024-05-04 15:05:19 -0700

    done with the Domcontent load

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-const changeBtn = document.getElementById('change-color-btn')
-const colorBox = document.getElementById('color-box')
+document.addEventListener("DOMContentLoaded", function(){
 
-function changeColor(){
-    const randomColor = Math.floor(Math.random()*16777215).toString(16);
-    colorBox.style.backgroundColor = `#${randomColor}`
-}
-changeBtn.addEventListener('click', () => {
-   changeColor()
+    const changeBtn = document.getElementById('change-color-btn');
+    const colorBox = document.getElementById('color-box');
+    
+    function getRandomColor(){
+        const randomColor = Math.floor(Math.random()*16777215).toString(16);
+        colorBox.style.backgroundColor = `#${randomColor}`
+    }
+    changeBtn.addEventListener('click', () => {
+        getRandomColor()
+    })
 })
